0bde80f4e118ae29adb176307e7582f73e66b2ac
kimberlythomas <thomas.kimberlym@gmail.com>
2022-06-24 18:16:41 -0700

    Refactoring

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@ let inputQuestion = document.getElementById("input");
 let question = document.getElementById("question");
 let answers = document.getElementById("answers");
 let eightBall = document.getElementById("eightBall");
+let randomAnswer = [
+  "img/magic8ball_1.png",
+  "img/magic8ball_2.png",
+  "img/magic8ball_3.png",
+  "img/magic8ball_4.png",
+  "img/magic8ball_5.png",
+  "img/magic8ball_6.png",
+  "img/magic8ball_7.png",
+  "img/magic8ball_8.png",
+  "img/magic8ball_9.png",
+  "img/magic8ball_10.png",
+  "img/magic8ball_11.png",
+  "img/magic8ball_12.png",
+  "img/magic8ball_13.png",
+  "img/magic8ball_14.png",
+  "img/magic8ball_15.png",
+  "img/magic8ball_16.png",
+  "img/magic8ball_17.png",
+  "img/magic8ball_18.png",
+  "img/magic8ball_19.png",
+];
 
 button.addEventListener("click", function () {
   console.log(input);
@@ -11,28 +32,6 @@ button.addEventListener("click", function () {
   if (input.value.length < 1) {
     alert("Please enter a question!");
   } else {
-    //Get the possible answers
-    let randomAnswer = [
-      "img/magic8ball_1.png",
-      "img/magic8ball_2.png",
-      "img/magic8ball_3.png",
-      "img/magic8ball_4.png",
-      "img/magic8ball_5.png",
-      "img/magic8ball_6.png",
-      "img/magic8ball_7.png",
-      "img/magic8ball_8.png",
-      "img/magic8ball_9.png",
-      "img/magic8ball_10.png",
-      "img/magic8ball_11.png",
-      "img/magic8ball_12.png",
-      "img/magic8ball_13.png",
-      "img/magic8ball_14.png",
-      "img/magic8ball_15.png",
-      "img/magic8ball_16.png",
-      "img/magic8ball_17.png",
-      "img/magic8ball_18.png",
-      "img/magic8ball_19.png",
-    ];
     //Display a random answer
     let randomNumber = Math.floor(Math.random() * 19);
     eightBall.src = randomAnswer[randomNumber];
@@ -50,7 +49,7 @@ const startOver = function () {
 };
 
 //Ask another question
-askAgain.addEventListener("click", function () {
+askAgain.addEventListener("click", function () {x
   inputQuestion.classList.remove("hide");
   askAgain.classList.add("hide");
   button.classList.remove("hide");
@@ -60,28 +59,6 @@ askAgain.addEventListener("click", function () {
   if (input.value.length < 1) {
     alert("Please enter a question!");
   } else {
-    //Get the possible answers
-    let randomAnswer = [
-      "img/magic8ball_1.png",
-      "img/magic8ball_2.png",
-      "img/magic8ball_3.png",
-      "img/magic8ball_4.png",
-      "img/magic8ball_5.png",
-      "img/magic8ball_6.png",
-      "img/magic8ball_7.png",
-      "img/magic8ball_8.png",
-      "img/magic8ball_9.png",
-      "img/magic8ball_10.png",
-      "img/magic8ball_11.png",
-      "img/magic8ball_12.png",
-      "img/magic8ball_13.png",
-      "img/magic8ball_14.png",
-      "img/magic8ball_15.png",
-      "img/magic8ball_16.png",
-      "img/magic8ball_17.png",
-      "img/magic8ball_18.png",
-      "img/magic8ball_19.png",
-    ];
     //Display a random answer
     let randomNumber = Math.floor(Math.random() * 19);
     eightBall.src = randomAnswer[randomNumber];
